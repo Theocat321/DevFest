1734d8277a589f56445f01ec491b248a0a562c62
Adam O'Neill <adamo.mark25@gmail.com>
2023-12-07 20:16:14 +0000

    checks if user in basket before adding

--- frontend/src/app/pages/single-product/single-product-page/single-product-page.component.ts
+++ frontend/src/app/pages/single-product/single-product-page/single-product-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { BrowserDetailsService } from 'src/app/services/browser-details.service';
+import { BrowserStorageService } from 'src/app/services/browser-storage.service';
 import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
@@ -22,7 +23,8 @@ export class SingleProductPageComponent implements OnInit{
   get quantity() { return this.quantityForm.get('qt')!; }
 
 
-  constructor(private route: ActivatedRoute, private ProductsClient:ProductsService,private formBuilder: FormBuilder,) {}
+  constructor(private route: ActivatedRoute, private ProductsClient:ProductsService,private formBuilder: FormBuilder,
+              private bStorage:BrowserStorageService) {}
 
   ngOnInit() {
     // Get the product id from url
@@ -37,18 +39,23 @@ export class SingleProductPageComponent implements OnInit{
           this.valid_id = true;
         }
       }
-      console.log(this.product_info)
-    })
-    console.log(this.product_info);
-    
+    })    
   }
 
   onSubmit(): void {
-    // Ask service to add to basket
-    let quantityInt = parseInt(this.quantity.value!)
-    let costPerUnit = parseFloat(this.product_info['cost_per_unit'])
-    this.ProductsClient.addProductToBasket(this.product_id,quantityInt,costPerUnit);
-    alert('Your items have been added to the basket');
-    this.quantityForm.reset();
+    // Check user in basket
+    let pin = this.bStorage.getSessionStorage("current_basket_pin")
+    let pw = this.bStorage.getSessionStorage("current_basket_hash")
+    if (pin != null && pw != null){
+      // Ask service to add to basket
+      let quantityInt = parseInt(this.quantity.value!)
+      let costPerUnit = parseFloat(this.product_info['cost_per_unit'])
+      this.ProductsClient.addProductToBasket(this.product_id,quantityInt,costPerUnit);
+      alert('Your items have been added to the basket');
+      this.quantityForm.reset();
+    }
+    else{
+      alert("You must join a basket")
+    }
   }
 }
