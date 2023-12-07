049ce78b19668c1109cbc01eab243a791a5c0fc2
Adam O'Neill <adamo.mark25@gmail.com>
2023-12-07 15:39:40 +0000

    background colour for your items is darker

--- frontend/src/app/cart/cart-item/cart-item.component.ts
+++ frontend/src/app/cart/cart-item/cart-item.component.ts
@@ -1,11 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BrowserDetailsService } from 'src/app/services/browser-details.service';
 
 @Component({
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
   styleUrls: ['./cart-item.component.scss']
 })
-export class CartItemComponent {
+export class CartItemComponent implements OnInit{
   @Input() item:any;
+
+  myItem:boolean = false
+
+  constructor(private bDetails:BrowserDetailsService){}
+
+  async ngOnInit() {
+    let fingerprint =await  this.bDetails.getBrowserFingerprint() 
+    if(this.item['user_added']==fingerprint){
+      this.myItem = true;
+    }
+  }
 
 }
